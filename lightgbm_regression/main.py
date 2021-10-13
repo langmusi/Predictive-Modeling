b2a82a55e4723fc4498276e552247f0d33250cc3
Min Liu <Min.Liu@atkinsglobal.com>
2021-10-13 22:13:36 +0200

    update

--- lightgbm_regression/main.py
+++ lightgbm_regression/main.py
@@ -6,5 +6,7 @@ from utils import DataLoader, DataProcessor
 data_dir = "C:/Users/LIUM3478/OneDrive Corp/OneDrive - Atkins Ltd/Work_Atkins/Docker/hjulanalys/wheel_prediction_data.csv"
 data_loader = DataLoader(data_dir)
 df = data_loader.load_data()
+
+pd.read_csv(data_dir, encoding = 'ISO 8859-1', sep = ";", decimal=",")
 df.shape
-data_load.load_data(data_path=)
+
