e490bdc64c81822d380c98196c45972b13c1c74e
Min Liu <Min.Liu@atkinsglobal.com>
2021-10-24 20:36:25 +0200

    brake discs position analysis

---
+++ Broms/docs/brake_docs.md
@@ -0,0 +1,13 @@
+1. bromskivor = brake disc
+
+One wheel has two brake discs.
+
+2. Two brake discs at one side of a train.
+One brake is located at the inside of the wheel, the other one at the outside of the wheel. 
+
+3. Important variables: 
+<https://www.researchgate.net/publication/342665784_Performance_analysis_of_brake_discs_in_trains>
+
+- Brakes' position
+- If a train stops many times, the number of stops impacts wear of brakes.
+- Tracks of trains and the types of trains' usage. The curves make a train slowing down which impacts brakes. If a train alreay moves slowly, then a curve track will not have much impact on brake wear.
--- Broms/eda.py
+++ Broms/eda.py
@@ -1,6 +1,12 @@
 from utils import DataLoader, DataProcessor, FeatureSelect, my_plot
 
-data_dir = 'brake_data_azure.csv'
+import os
+for dirname, _, filenames in os.walk('./data'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+data_dir = './data/brake_data_azure.csv'
 data_loader = DataLoader()
 df = data_loader.read_data(data_dir)
 #df = data_loader.ImportAndCleanSQL(dataquery="select * from BrakeData")
@@ -53,6 +59,29 @@ plt.show()
 df[df['BrskChangeDate1'].isnull() & df['BrskChangeDate2'].notnull()]
 df[df['BrskChangeDate2'].isnull() & df['BrskChangeDate1'].notnull()]
 
+
+###########################################
+#### brake discs located at the right, left sides of a train #######
+df['ComponentParentLocation'].unique()
+df[df['ComponentParentLocation'] ==2]
+df[df['ComponentParentLocation'].isna()]
+
+mask = (df['ComponentParentLocation'] == 1) & (df['BrskChangeDate1'].notnull())
+col_names = ['BrskThickness1', 'TotalPerformanceSnapshot']
+thick_1 = df.loc[mask, col_names]
+
+df_sort = df.sort_values('ComponentParentLocation')
+
+cols = ['BrskThickness1', 'BrskThickness2', 'BrskThickness3', 'BrskThickness4']
+data_prep = DataProcessor(df)
+df_1 = data_prep.string_to_numeric(df, cols=cols)
+
+def mu_sd(df, col_1):
+    df.groupby('ComponentParentLocation').agg({col_1: ['mean', 'std']})
+
+df_1.groupby('ComponentParentLocation').agg({'BrskThickness1': ['mean', 'std']})
+df_1.groupby('ComponentParentLocation')[cols].describe()
+##########################################
 
 columns_to_remove = ['PostID', 'BrskLatheDate1', 'BrskLatheDate2',
                     'BrskLatheDate3', 'BrskLatheDate4',
